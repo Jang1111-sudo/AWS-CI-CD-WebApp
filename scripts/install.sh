1b261a7a0404d44ab288570f2e9a13c9f0fc640f
jang111 <jangty0@naver.com>
2026-02-28 09:32:08 +0900

    Fix install.sh again

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
-if ! rpm -q nginx; then
-	dnf install -y nginx
+if rpm -q nginx; then
+	systemctl stop nginx || true
+	dnf remove -y nginx || true
 fi
+
+rm -rf /var/cache/dnf/* || true
+rm -f /var/lib/rpm/__db* || true
+rpm --rebuilddb || true
+
+dnf -y install nginx
+
 mkdir -p /usr/share/nginx/html
 systemctl enable nginx
 systemctl start nginx
