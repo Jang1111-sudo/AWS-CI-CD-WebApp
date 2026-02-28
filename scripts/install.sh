6d9fb47eb79709a7d4513f47f081fc5170f30698
jang111 <jangty0@naver.com>
2026-02-28 21:03:44 +0900

    Fix install.sh using reinstall

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-echo "===install.sh start==="
-date
-whoami
-pwd
-ls -al
-
-#install nginx : if already installed it, skip
-if ! rpm -q nginx > /dev/null 2>&1; then
-	dnf clean all || true
-	rm -rf /var/cache/dnf/* || true
-	dnf -y install nginx
-else
-	echo "nginx already installed"
-fi
-
-mkdir -p /usr/share/nginx/html
-#there could not be nginx user. -> even if you failed it, continue
-id nginx || true
-chown -R nginx:nginx /usr/share/nginx/html || true
+dnf -y reinstall nginx
 systemctl enable nginx
 systemctl start nginx
