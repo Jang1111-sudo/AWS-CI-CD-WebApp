6194ad87b054dd883ef38cc334709aedb3521364
jang111 <jangty0@naver.com>
2026-03-02 10:40:44 +0900

    modified install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 set -euxo pipefail
 
-#1) nginx already installed -> reinstall for recover file corruption or missing
-#2) nginx not installed -> newly install
-if rpm -q nginx > /dev/null 2>&1; then
-	dnf -y reinstall nginx
-else
-	dnf -y install nginx
-fi
+#install nginx(if it isn't -> install, if it is -> continue)
+dnf -y install nginx nginx-core nginx-filesystem
 
 #ready for web root
 mkdir -p /usr/share/nginx/html
 
+#if index.html is directory, remove it
+if [ -d /usr/share/nginx/html/index.html]; then
+	rm -rf /usr/share/nginx/html/index.html
+fi
+
+#ensure /etc/nginx
+mkdir -p /etc/nginx
+
+#if there is not mime.types, make
+if [ ! -f /etc/nginx/mime.types ]; then
+	cat >/etc/nginx/mime.types <<'EOF'
+types {
+    text/html                             html htm;
+    text/css                              css;
+    text/xml                              xml;
+    image/gif                             gif;
+    image/jpeg                            jpeg jpg;
+    application/javascript                js;
+    application/json                      json;
+    application/pdf                       pdf;
+    application/octet-stream              bin exe;
+    image/png                             png;
+    image/svg+xml                         svg;
+}
+EOF
+fi
+#start nginx after check 
+nginx -t
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
